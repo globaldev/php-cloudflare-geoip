06a1ed3bd67a5cbe79c9a456c638c80af908c431
Calum Roberts <croberts@whitelabeldating.com>
2017-09-12 12:44:24 +0100

    Replace file_exists with require

--- index.example.php
+++ index.example.php
@@ -1,16 +1,8 @@
 <?php
-if (file_exists("cloudflare-geoip/geoip.php")) {
-	include "cloudflare-geoip/geoip.php";
-	$geoip = new Geoip();
-	$country = (isset($_SERVER["HTTP_CF_IPCOUNTRY"]) ? $_SERVER["HTTP_CF_IPCOUNTRY"] : "GB");
-	$geoip->country_check($country);
-	define("WLD_SITE_URL", "http://" . APP_URL);
-} else {
-	define("WLD_SITE_ID", DEFAULT_COUNTRY['WLD_SITE_ID']);
-	define("WLD_SITE_URL", "http://" . DEFAULT_COUNTRY['WLD_SITE_URL']);
-	define("WLD_COUNTRY_CODE", DEFAULT_COUNTRY_CODE);
-	define("WLD_COUNTRY_NAME", DEFAULT_COUNTRY_NAME);
-}
+require 'cloudflare-geoip/geoip.php';
+$geoip = new Geoip();
+$country = (isset($_SERVER["HTTP_CF_IPCOUNTRY"]) ? $_SERVER["HTTP_CF_IPCOUNTRY"] : "GB");
+$geoip->country_check($country);
 
 define("WLD_SSL_APP_URL", "https://secure2.whitelabeldating.com");
 define("WLD_STANDARD_SIGNUP_FORM", WLD_SITE_URL . "/signup.cfm");
