df2b64dfb40c27d7fb8459b290bd3548cddd559b
Calum Roberts <croberts@whitelabeldating.com>
2017-09-12 12:45:12 +0100

    Add http protocol to non ssl sign up url

--- index.example.php
+++ index.example.php
@@ -5,6 +5,6 @@ $country = (isset($_SERVER["HTTP_CF_IPCOUNTRY"]) ? $_SERVER["HTTP_CF_IPCOUNTRY"]
 $geoip->country_check($country);
 
 define("WLD_SSL_APP_URL", "https://secure2.whitelabeldating.com");
-define("WLD_STANDARD_SIGNUP_FORM", WLD_SITE_URL . "/signup.cfm");
+define("WLD_STANDARD_SIGNUP_FORM", "http://" . WLD_SITE_URL . "/signup.cfm");
 define("WLD_SSL_SIGNUP_FORM", WLD_SSL_APP_URL . "/signup.cfm?wldsite=" . WLD_SITE_ID);
 ?>
